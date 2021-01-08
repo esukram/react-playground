2c2b4d0dadf9e4aee24e27f8cf4a0e2649006dc5
Markus Heimbach <markus@esukram.org>
2021-01-08 19:53:37 +0100

    Rectoring of navigation component.

--- cra-vanilla/src/App.test.tsx
+++ cra-vanilla/src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import {BrowserRouter as Router } from 'react-router-dom';
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<Router><App /></Router>);
+
+  expect(screen.getByText(/Home Page/i))
+    .toBeInTheDocument();
 });
