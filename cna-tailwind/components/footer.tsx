159d826daf4c9f0ab7a6a110a6a57c37b9d0fea7
Markus Heimbach <markus@esukram.org>
2021-01-10 16:28:08 +0100

    Run Prettier...

--- cna-tailwind/components/footer.tsx
+++ cna-tailwind/components/footer.tsx
@@ -1,15 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import styles from './footer.module.css'
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <ul>
-        <li><Link href="/impressum" as="impressum">Impressum</Link></li>
+        <li>
+          <Link href="/impressum" as="impressum">
+            Impressum
+          </Link>
+        </li>
       </ul>
     </footer>
   );
-}
+};
 
 export default Footer;
--- cna-tailwind/components/nav.tsx
+++ cna-tailwind/components/nav.tsx
@@ -1,22 +1,28 @@
-import Link from 'next/link'
-import styles from './nav.module.css'
+import Link from "next/link";
+import styles from "./nav.module.css";
 
 const Nav = () => {
   return (
     <nav className={styles.navigation}>
       <ul>
         <li>
-          <Link href="/" as="">Home</Link>
+          <Link href="/" as="">
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/foo" as="foo">Foo</Link>
+          <Link href="/foo" as="foo">
+            Foo
+          </Link>
         </li>
         <li>
-          <Link href="/bar"><a>Bar</a></Link>
+          <Link href="/bar">
+            <a>Bar</a>
+          </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Nav;
